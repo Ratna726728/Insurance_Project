82cea8aa258e2dd1810dda95af1774d3d62bb293
Ratnadeep <ratna726728@gmail.com>
2023-05-11 16:01:30 +0530

    Adding controller to branch

---
+++ InsuranceProject3/src/main/java/com/amdocs/controller/PolicyPremiumInsertController.java
@@ -0,0 +1,13 @@
+package com.amdocs.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.amdocs.service.PolicyPremiumService;
+
+@RestController
+public class PolicyPremiumInsertController {
+	
+//	inject the service
+	private PolicyPremiumService policyPremiumService;
+
+}
