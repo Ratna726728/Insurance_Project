0627fbe4ff64ca7066c02aec4a3daa5b096529aa
Ratna <ratna726728@gmail.com>
2023-05-12 16:15:10 +0530

    done some minor changes

--- InsuranceProject3/src/main/java/com/amdocs/InsuranceProject3Application.java
+++ InsuranceProject3/src/main/java/com/amdocs/InsuranceProject3Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class InsuranceProject3Application {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(InsuranceProject3Application.class, args);
 	}
 
