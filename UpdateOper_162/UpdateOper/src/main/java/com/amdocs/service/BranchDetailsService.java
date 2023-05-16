084f5022dc05714f6b2c476d6e1097a9646ec9f6
Ratna <ratna726728@gmail.com>
2023-05-16 13:38:59 +0530

    Updating names for classes and adding service

---
+++ UpdateOper_162/UpdateOper/src/main/java/com/amdocs/service/BranchDetailsService.java
@@ -0,0 +1,8 @@
+package com.amdocs.service;
+
+import com.amdocs.model.BranchDetails;
+
+public interface BranchDetailsService {
+	public BranchDetails updateBranchDetails(BranchDetails branchDetails); 
+
+}
