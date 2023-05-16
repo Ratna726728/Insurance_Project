41b43c6ee2526f158023227d247ec2c4fce0a9b6
Ratna <ratna726728@gmail.com>
2023-05-16 13:39:59 +0530

    adding modified files

--- UpdateOper_162/UpdateOper/src/main/java/com/amdocs/BranchDetailsApplication.java
+++ UpdateOper_162/UpdateOper/src/main/java/com/amdocs/BranchDetailsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UpdateOperApplication {
+public class BranchDetailsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UpdateOperApplication.class, args);
+		SpringApplication.run(BranchDetailsApplication.class, args);
 	}
 
 }
--- UpdateOper_162/UpdateOper/src/main/java/com/amdocs/model/BranchDetails.java
+++ UpdateOper_162/UpdateOper/src/main/java/com/amdocs/model/BranchDetails.java
@@ -8,7 +8,7 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name= "branch_details")
-public class UpdateDetails {
+public class BranchDetails {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
