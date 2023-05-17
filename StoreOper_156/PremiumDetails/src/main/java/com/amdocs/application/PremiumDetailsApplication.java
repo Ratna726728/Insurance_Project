06e10b5e70bb4644d236330c9cfbf3629133163c
Ratna <ratna726728@gmail.com>
2023-05-17 14:58:53 +0530

    Adding Store operation in Insurance Project of task number 156

---
+++ StoreOper_156/PremiumDetails/src/main/java/com/amdocs/application/PremiumDetailsApplication.java
@@ -0,0 +1,13 @@
+package com.amdocs.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PremiumDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PremiumDetailsApplication.class, args);
+	}
+
+}
