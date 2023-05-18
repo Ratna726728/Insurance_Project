c324fc692547641776437e769b57ea0327a936a3
Archana kute <archanakute2504@gmail.com>
2023-05-18 17:25:43 +0530

    adding update operation of task 154

---
+++ UpdateInsuranceTask_154/src/main/java/com/velocity/service/UpdateInsuranceService.java
@@ -0,0 +1,9 @@
+package com.velocity.service;
+
+import com.velocity.model.UpdateInsuranceModel;
+
+public interface UpdateInsuranceService {
+	
+	public UpdateInsuranceModel updateData(UpdateInsuranceModel updateInsuranceModel);
+
+}
