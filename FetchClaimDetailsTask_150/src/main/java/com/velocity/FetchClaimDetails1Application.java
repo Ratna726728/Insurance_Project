a9004936ef1b324f5c4d2d2158454b3b5dab6b38
Archana kute <archanakute2504@gmail.com>
2023-05-24 16:02:03 +0530

    Fetching details task-150

---
+++ FetchClaimDetailsTask_150/src/main/java/com/velocity/FetchClaimDetails1Application.java
@@ -0,0 +1,13 @@
+package com.velocity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FetchClaimDetails1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FetchClaimDetails1Application.class, args);
+	}
+
+}
---
+++ FetchClaimDetailsTask_150/src/main/java/com/velocity/service/FetchClaimDetailsService.java
@@ -0,0 +1,8 @@
+package com.velocity.service;
+
+import com.velocity.model.FetchClaimDetailsModel;
+
+public interface FetchClaimDetailsService {
+
+	public FetchClaimDetailsModel getDetails(Integer id);
+}
